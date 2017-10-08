7a68e8e75e7f93486f574f067b123d8eba632bfa
anboo <admin@rusdteam.ru>
2017-10-08 18:08:34 +0200

    Fix dist bin file for .gitignore

---
+++ test/caller_test.go
@@ -0,0 +1,20 @@
+package test
+
+import (
+	"testing"
+
+	baloo "gopkg.in/h2non/baloo.v2"
+)
+
+// test stores the HTTP testing client preconfigured
+var test = baloo.New("http://httpbin.org")
+
+func TestBalooJSONAssertion(t *testing.T) {
+	test.Get("/user-agent").
+		SetHeader("Foo", "Bar").
+		Expect(t).
+		Status(200).
+		Type("json").
+		JSON(`{"user-agent":"baloo/` + baloo.Version + `"}`).
+		Done()
+}
